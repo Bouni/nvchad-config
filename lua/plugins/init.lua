1a85ad5b95d170fd5b27ddaef8af8145da35333b
Bouni <bouni@owee.de>
2025-08-22 11:54:45 +0200

    Add brenoprata10/nvim-highlight-colors, update all plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,11 +14,41 @@ return {
   },
 
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc", "html", "css", "python", "markdown"
-  		},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+        "markdown",
+      },
+    },
+  },
+
+  {
+    "brenoprata10/nvim-highlight-colors",
+    event = "BufReadPre",
+    config = function()
+      require("nvim-highlight-colors").setup {
+        -- Render style
+        render = "background", -- or 'foreground' or 'virtual'
+
+        -- Set virtual symbol (only used in virtual render mode)
+        virtual_symbol = "■",
+
+        -- Highlight named colors, like 'green'
+        enable_named_colors = true,
+
+        -- Highlight tailwind colors, e.g. 'bg-blue-500'
+        enable_tailwind = false,
+
+        -- Exclude filetypes or buftypes from being highlighted
+        exclude_filetypes = {},
+        exclude_buftypes = {},
+      }
+    end,
   },
 }
