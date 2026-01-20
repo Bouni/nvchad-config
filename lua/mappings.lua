11e363c4fb74f43925b822a401eea3e81e1749f1
bouni <bouni@owee.de>
2026-01-20 11:27:41 +0100

    Disable q for macro recording

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "q", "<nop>", { desc = "Disable macro recording" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
